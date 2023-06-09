f122a318729113aaec17bcafd64c288313cc87c8
Jordan Ford <jsford94@gmail.com>
2023-06-09 09:53:29 -0400

    Python site generator sorta works to convert markdown to html.

---
+++ posts/0001-lidar-compression/lidar-compression.md
@@ -0,0 +1,5 @@
+---
+title: Something about LiDAR Odometry and Mapping...
+description: do something cool!
+date: June 9, 2023
+---
--- tools/build-site.py
+++ tools/build-site.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 import os
-<<<<<<< HEAD
 import glob
 import shutil
 import os.path as osp
 import markdown
+import yaml
 import jinja2
+from datetime import datetime
 
 
 def replace_dir(path):
     if osp.exists(path):
         shutil.rmtree(path)
     os.mkdir(path)
+
+
+def change_extension(path, new_ext):
+    base = osp.splitext(path)[0]
+    return base + '.' + new_ext
 
 
 def copytree(src, dst):
@@ -41,57 +47,87 @@ def list_post_dirs(site_root, posts_root='posts'):
     return post_dirs
 
 
+class BlogPost:
+    def __init__(self, directory, mdfilename, title, description, date, html):
+        self.directory = directory
+        self.mdfilename = mdfilename
+        self.htmlfilename = change_extension(mdfilename, 'html')
+        self.title = title
+        self.description = description
+        self.date = date
+        self.html = html
+
+
 def build_site(site_root, site_dirname):
 
+    # Construct a build directory for the site.
     site_dir = osp.join(site_root, site_dirname)
     replace_dir(site_dir)
 
+    # Copy blog post directories into the build tree.
     posts_dirname = 'posts'
     posts_src_dir = osp.join(site_root, posts_dirname)
     posts_dst_dir = osp.join(site_dir, posts_dirname)
     copytree(posts_src_dir, posts_dst_dir)
 
+    # Copy site assets into the build tree.
     assets_dirname = 'assets'
     assets_src_dir = osp.join(site_root, assets_dirname)
     assets_dst_dir = osp.join(site_dir, assets_dirname)
     copytree(assets_src_dir, assets_dst_dir)
 
+    # Copy css into the build tree.
     css_dirname = 'css'
     css_src_dir = osp.join(site_root, css_dirname)
     css_dst_dir = osp.join(site_dir, css_dirname)
     copytree(css_src_dir, css_dst_dir)
 
+    # Copy the index.html page into the build tree.
     copyfile(osp.join(site_root, 'templates/index.html'), site_dir)
 
-    # Construct the blog.html page.
-    environment = jinja2.Environment()
-    blog_template = environment.from_string( read_file(osp.join(site_root, 'templates/blog.html') ))
-
+    # Find all of the blog posts.
     post_dirs = list_post_dirs(site_root, posts_dirname)
+    posts = []
+    for directory in post_dirs:
+        mdfiles = glob.glob('*.md', root_dir=directory)
+        if len(mdfiles) != 1:
+            print("Error: Blog post directory '{directory}' must contain exactly one markdown file.")
+        mdfile = mdfiles[0]
+
+        postcontent = read_file(osp.join(directory, mdfile))
+        splitcontent = postcontent.split('---', maxsplit=2)
+        yamlcontent = splitcontent[1]
+        mdcontent = splitcontent[2]
+        htmlcontent = markdown.markdown(mdcontent)
+
+        header = yaml.load(yamlcontent, Loader=yaml.FullLoader)
+        post = BlogPost(directory, mdfile, header['title'], header['description'], header['date'], htmlcontent)
+        posts.append(post)
+    posts.sort(key=lambda p : datetime.strptime(p.date, '%B %d, %Y'), reverse=True)
+
+    # Construct the blog.html page and add it to the build tree.
+    environment = jinja2.Environment()
+    blog_template_string = read_file(osp.join(site_root, 'templates/blog.html'))
+    blog_template = environment.from_string( blog_template_string )
+
+    posts_list = []
+    for post in posts:
+        postpath = osp.join('posts', post.directory.split('/')[-1], post.htmlfilename)
+        posts_list.append([postpath, post.title, post.date, post.description])
     
-    blog_page = blog_template.render(posts=[['#','b','c'], ['#','e','f']])
+    blog_page = blog_template.render(posts=posts_list)
     write_file(osp.join(site_dir, 'blog.html'), blog_page)
+
+    # Construct a page for each blog post and add it to the build tree.
+    for post in posts:
+        postpath = osp.join(site_dir, post.htmlfilename)
+        post_template_string = read_file(osp.join(site_root, 'templates/post.html'))
+        post_template = environment.from_string( post_template_string )
+
+        post_page = post_template.render(title=post.title, contents=post.html)
+        postpath = osp.join('posts', post.directory.split('/')[-1], post.htmlfilename)
+        write_file(osp.join(site_dir, postpath), post_page)
 
 
 if __name__=='__main__':
     build_site('.', 'site')
-=======
-import os.path as osp
-import markdown
-
-def build_post(post_root):
-    print(post_root)
-
-def build_site(site_root):
-    # Create build directory.
-
-    # Build blog posts.
-    posts = os.listdir(osp.join(site_root, 'posts'))
-    for post in posts:
-        build_post(post)
-
-    # Copy everything into the build directory.
-
-if __name__=='__main__':
-    build_site('.')
->>>>>>> 7c1a9a36e7ed3ea76e998217b7c92ccd7f2ba589
